ad18cce6a4ab474002cef6076a28ffff0ff48597
Jasper Swensen <jasperswensen@gmail.com>
2023-07-05 23:54:33 -0600

    toolbar

--- app/src/BeanToolbar.js
+++ app/src/BeanToolbar.js
@@ -6,16 +6,16 @@ import MenuIcon from '@mui/material/MenuItem';
 
 function BeanToolbar() {
   return (
-    <div className="App">
-    <Toolbar style={{color: 'black', backgroundColor: '#bbbbbb'}}>
-        <img src={bean_with_text} className="App-logo" alt="logo" />
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          
-        </Typography>
-      <Button style={{color: 'black', backgroundColor: '#e7e7e7', margin: '5px', height: '5vmin', width: '10vmin'}}>Gallery</Button>
-      <Button style={{color: 'black', backgroundColor: '#e7e7e7', margin: '5px', height: '5vmin', width: '10vmin'}}>About</Button>
-      <Button style={{color: 'black', backgroundColor: '#e7e7e7', margin: '5px', height: '5vmin', width: '10vmin'}}>Login</Button>
-    </Toolbar>
+    <div >
+        <Toolbar style={{color: 'black', backgroundColor: '#bbbbbb'}}>
+            <img src={bean_with_text} className="App-logo" alt="logo" />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            
+            </Typography>
+        <Button style={{color: 'black', backgroundColor: '#e7e7e7', margin: '5px', height: '5vmin', width: '10vmin'}}>Gallery</Button>
+        <Button style={{color: 'black', backgroundColor: '#e7e7e7', margin: '5px', height: '5vmin', width: '10vmin'}}>About</Button>
+        <Button style={{color: 'black', backgroundColor: '#e7e7e7', margin: '5px', height: '5vmin', width: '10vmin'}}>Login</Button>
+        </Toolbar>
     </div>
   );
 }
