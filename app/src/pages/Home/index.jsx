751d90fb477b9b1497a5e59f3c83feceb2c97a08
Jasper Swensen <jasperswensen@gmail.com>
2023-07-11 20:08:42 -0600

    changes

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ function HomePage() {
       <BeanNavbar />
       <div className="front-page">
         <div class="front-page-container">
-          <Typography variant="h1">Full coverage. Guaranteed.</Typography>
+          <h1 className="front-page-header">Full coverage. Guaranteed.</h1>
           <img src={process.env.PUBLIC_URL + '/photos/rim2.jpeg'} className='front-page-image'></img>
           <p1 className="front-page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere leo eu volutpat ultricies. Quisque pharetra magna eget ex posuere rutrum. Proin dapibus, lacus quis suscipit commodo, felis augue efficitur sapien, quis sagittis lectus risus ut neque. Pellentesque quis leo imperdiet purus commodo pretium sed ut risus. Sed blandit enim eu tellus mattis efficitur.</p1>
         </div>
